61f36f5025e110e4711ff28c3b472015b5ca5288
jeffyactive <jeff@reelyactive.com>
2016-12-29 23:00:24 -0500

    Seeded quotes

---
+++ js/jeffyactive.js
@@ -0,0 +1,17 @@
+const QUOTES = [ 'Fuller', 'Hidalgo', 'Wiener' ];
+const CYCLE_MILLISECONDS = 30000;
+
+angular.module('jeffyactive', [])
+
+  .controller('QuoteCtrl', function($scope, $interval) {
+    $scope.quote = randomQuote();
+
+    function randomQuote() {
+      var randomIndex = Math.floor(Math.random() * QUOTES.length);
+      return QUOTES[randomIndex];
+    }
+
+    var cycle = $interval(function() {
+      $scope.quote = randomQuote();
+    }, CYCLE_MILLISECONDS);
+  });
